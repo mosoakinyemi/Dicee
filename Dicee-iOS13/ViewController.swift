ed59753e8b24f3d7696d5649acc095889b31ad9a
Akinyemi Mosolasi <akinyemimosolasi@gmail.com>
2020-05-19 13:53:20 +0100

    Revert "Completed Dice UI"
    This reverts commit c020bd44ee8ff1b67d2d7a1677ce4f943ab305b2.

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -3,7 +3,6 @@
 //  Dicee-iOS13
 //
 //  Created by Angela Yu on 11/06/2019.
-//  Implemented by Akinyemi Mosolasi 18/05/2020
 //  Copyright © 2019 London App Brewery. All rights reserved.
 //
 
@@ -13,16 +12,12 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var diceImageView2: UIImageView!
     @IBOutlet weak var diceImageView1: UIImageView!
-        
     override func viewDidLoad() {
         super.viewDidLoad()
+        diceImageView1.image = #imageLiteral(resourceName: "DiceSix")
+        diceImageView2.image = #imageLiteral(resourceName: "DiceFour")
     }
 
-    @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageView1.image = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")][Int.random(in: 0..<6)]
-        diceImageView2.image = [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")][Int.random(in: 0..<6)]
-    }
-    
+
 }
 
- 
